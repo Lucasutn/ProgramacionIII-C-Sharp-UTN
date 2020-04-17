923e75ed34def26ec1456dcae39155d7f124b930
Lucas <lucashnutn@gmail.com>
2020-04-17 14:31:15 -0300

    v1

---
+++ Dado/Dado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dado
+{
+    public class Dado
+    {
+        private int numbers;
+
+        public Dado(int numbers)
+        {
+            this.numbers = numbers;
+        }
+
+
+        public int lanzar()
+        {
+            return new Random().Next(numbers) + 1;
+        }
+    }
+}
---
+++ Dado/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Dado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey imput = ConsoleKey.Enter;
+
+            Dado dado = new Dado(6);
+
+
+            while (imput.Equals(ConsoleKey.Enter))
+            {
+                WriteLine("Ingrese apuesta 1-6 \n");
+
+
+                bool resul = play(int.Parse(Console.ReadLine()), dado);
+
+
+                if (resul)
+                {
+                    WriteLine("Ganador \n");
+                }
+                else
+                {
+                    WriteLine("Perdedor \n");
+                }
+
+                WriteLine("\n Enter para Continuar::\n");
+                imput = ReadKey().Key;
+            }
+        }
+
+
+        private static bool play(int apuesta, Dado dado)
+        {
+            
+            return apuesta == dado.lanzar();
+        }
+    }
+}
